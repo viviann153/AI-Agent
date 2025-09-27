bb296746d4a4dfba45721d3bb3f9fc8102477d89
vivian1539 <viviann@vt.edu>
2025-09-27 13:51:29 -0400

    main

---
+++ main.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+
+load_dotenv()
+
+class Response(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+parser = PydanticOutputParser(pydantic_object=Response)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are an housing assistant that will help students at the Virginia Polytechnic Institute 
+            and University find off-campus housing near campus, around Blacksburg Virginia. 
+            Answer the user query and use neccessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+
+    ]
+
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm, 
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the Capital of france?"})
+print(raw_response)
+
+#structured_response = parser.parse(raw_response.get("output")[0]["text"])
+#print(structured_response)
